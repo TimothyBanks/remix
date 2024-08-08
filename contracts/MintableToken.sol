ae633ae1db1bed98c641628afcb8aba419472326
TimothyBanks <timothyaaronbanks@gmail.com>
2024-08-07 20:19:29 -0400

    Working on mintabletoken

--- contracts/MintableToken.sol
+++ contracts/MintableToken.sol
@@ -19,22 +19,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    int256 private vote_duration;
-    int8 private quorum_size = 0;
+    uint256 private vote_duration;
+    uint8 private quorum_size = 0;
     mapping(address => bool) private parties;
-    int8 private party_count;
-
-    int32 private proposal_count = 0;
+    uint8 private party_count;
+    uint32 private proposal_count = 0;
+    bool private locked = false;
 
     struct proposal {
+        uint32 id;
         uint256 timestamp;
         address recipient;
         uint256 amount;
         mapping(address => bool) parties;
-        int8 votes;
+        uint8 votes;
     }
 
-    mapping(int32 => proposal) private proposals;
+    mapping(uint32 => proposal) private proposals;
+
+    event Proposal(uint32 id, address recipient, uint256 amount);
+    event Passed(uint32 id, address recipient, uint256 amount);
+    event Failed(uint32 id, address recipient, uint256 amount);
 
     constructor(address initialOwner)
         ERC20("MintableToken", "MT")
@@ -46,11 +51,18 @@ contract MyToken is ERC20, Ownable {
     modifier verifyAddress(address a) {
         require(a != address(0), "Invalid address");
         _;
-    } 
+    }
 
     modifier validParty(address a) {
         require(parties[a], "Invalid party");
         _;
+    }
+
+    modifier noReentry() {
+        require(!locked, "Reentry is not allowed");
+        locked = true;
+        _;
+        locked = false;
     }
 
     function addParty(address p) external onlyOwner verifyAddress(p) {
@@ -58,30 +70,46 @@ contract MyToken is ERC20, Ownable {
         party_count += 1;
     }
 
-    function setQuorumSize(int8 size) external onlyOwner {
+    function setQuorumSize(uint8 size) external onlyOwner {
         require(proposal_count == 1, "Active proposals");
         require(size <= party_count, "Invalid quorum size");
         quorum_size = size;
     }
 
-    function setVoteDuration(int256 duration) external onlyOwner {
+    function setVoteDuration(uint256 duration) external onlyOwner {
         require(proposal_count == 1, "Active proposals");
         vote_duration = duration;
     }
 
-    function proposeMint(address recipient_, uint256 amount_) external verifyAddress(recipient_) validParty(msg.sender) {
+    function proposeMint(address recipient_, uint256 amount_)
+        external
+        verifyAddress(recipient_)
+        validParty(msg.sender)
+    {
         require(quorum_size > 0, "No quorum specified");
         proposals[proposal_count].recipient = recipient_;
         proposals[proposal_count].amount = amount_;
         proposals[proposal_count].timestamp = block.timestamp;
+        proposals[proposal_count].id = proposal_count;
+        emit Proposal(proposal_count, recipient_, amount_);
         proposal_count += 1;
     }
 
-    function gc(int32 begin, int32 end) external onlyOwner {
-
+    function gc(uint32 begin, uint32 end) external onlyOwner {
+        uint256 now_ = block.timestamp;
+        for (uint32 i = begin; i < end; ++i) {
+            if (proposals[i].recipient == address(0)) {
+                continue;
+            }
+            if (proposals[i].timestamp + vote_duration > now_) {
+                continue;
+            }
+            emit Failed(i, proposals[i].recipient, proposals[i].amount);
+            delete proposals[i];
+        }
     }
 
-    function vote(int32 id) external validParty(msg.sender) {
+    function vote(uint32 id) external validParty(msg.sender) noReentry {
         require(proposals[id].recipient != address(0), "Invalid proposal");
         require(!proposals[id].parties[msg.sender], "Already voted");
         proposals[id].parties[msg.sender] = true;
@@ -89,11 +117,8 @@ contract MyToken is ERC20, Ownable {
 
         if (proposals[id].votes >= quorum_size) {
             _mint(proposals[id].recipient, proposals[id].amount);
+            emit Passed(id, proposals[id].recipient, proposals[id].amount);
             delete proposals[id];
         }
     }
-
-    function pendingProposals() external view returns (mapping(int32 => proposal) memory) {
-        return proposals;
-    }
- }
+}
